5036c4380e283e70d3b381e07a16d61aa79be948
Geska-Assurances-et-Conseils <tbouchard@geska.ca>
2026-04-22 17:24:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ DONNÉES DES NOTES
 ------------------------- */
 
     const notesData = {
-        "Annulation": {
+        "Annulation Test": {
             template: `{{Type de communication}} {{Nom du client}}
 Annulation d'un contrat
 Type d'annulation : {{Type d'annulation}}
